6bbef74dced333a5bf363379d00a6dc3c21f997b
Shubham Tiwari <shubhmtiwri00@gmail.com>
2022-01-25 13:20:59 +0530

    Changes in 11-RestParameter.js

--- 11-RestParameter.js
+++ 11-RestParameter.js
@@ -6,15 +6,32 @@ function Product(...nums){
     }
     return product;
        
-   }
-   
-   console.log(Product(1,2,3,4,5))
-   
-   //object destructuring 
-   const colours = ["Red","Blue","Black","Violet","Green","Magenta"]
-   const [a,b,...args] = colours;
-   console.log(a)
-   console.log(b)
-   console.log(args)
-   
+}
+
+console.log(Product(1,2,3,4,5))
+
+//object destructuring 
+const colours = ["Red","Blue","Black","Violet","Green","Magenta"]
+const [a,b,...args] = colours;
+console.log(a)
+console.log(b)
+console.log(args)
+
+
+//object destructuring of functions return output
+const functionDestructure = () => {
+    return [100,200,300,400];
+}
+
+const [a,b,c,d] = functionDestructure();
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)
+
+// swapping values
+let x = 10, y = 20;
+
+[x , y] = [y , x];
+console.log(x , y)
    
