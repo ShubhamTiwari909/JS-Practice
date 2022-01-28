5b6ad38d330d4aad0b9385539c1e35502fce9d30
Shubham Tiwari <shubhmtiwri00@gmail.com>
2022-01-28 13:25:59 +0530

    Just formatting changes

--- 17-StringMethods.js
+++ 17-StringMethods.js
@@ -1,54 +1,53 @@
-const str = "Hello ES6 you are the best"
-console.log(str.length);//character counts
-console.log(str.split(" ").length);//word counts
+const str = "Hello ES6 you are the best";
+console.log(str.length); //character counts
+console.log(str.split(" ").length); //word counts
 
 //startwith method
-const startWith = str.startsWith("Hello")//true
+const startWith = str.startsWith("Hello"); //true
 console.log(startWith);
 
 //endswith method
-const endWith = str.endsWith("best")//true
-console.log(endWith)
+const endWith = str.endsWith("best"); //true
+console.log(endWith);
 
 //includes
 const include = str.includes("ES6");
-console.log(include);//return true
+console.log(include); //return true
 
 //repeat
-const Repeat = str.repeat(2)
-console.log(Repeat);//repeat eh string two times;
+const Repeat = str.repeat(2);
+console.log(Repeat); //repeat eh string two times;
 
 //charAt
 const CharAt = str.charAt(10);
-console.log(CharAt);//prints "y" - 10th index
+console.log(CharAt); //prints "y" - 10th index
 
-//concat 
+//concat
 const str2 = "ES6 Provided extra features to Javascript";
-console.log(str.concat(str2));//concates both the strings
+console.log(str.concat(str2)); //concates both the strings
 
 //match
 const Match = str.match("you");
 console.log(Match);
 
-
 //replace
-const Replace = str.replace("ES6","ES7");
-console.log(Replace);//replace the given string with new provided string
+const Replace = str.replace("ES6", "ES7");
+console.log(Replace); //replace the given string with new provided string
 
-//search 
+//search
 const Search = str.search("are");
-console.log(Search);//print 14 - the starting index of you
+console.log(Search); //print 14 - the starting index of you
 
 //value Of
 const ValueOf = str.valueOf();
-console.log(ValueOf);//prints the string value
+console.log(ValueOf); //prints the string value
 
 //slice
-const Slice = str.slice(6,10);
-console.log(Slice);//print value from index 6 to index 9 
+const Slice = str.slice(6, 10);
+console.log(Slice); //print value from index 6 to index 9
 
 //split
-console.log(str.split(" ").length);//split the string on "white_spaces" and make it an array and return the array length - word counts
+console.log(str.split(" ").length); //split the string on "white_spaces" and make it an array and return the array length - word counts
 
 //Lower and Upper case
 console.log(str.toLowerCase());
